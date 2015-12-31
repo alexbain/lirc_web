f2f86632763bf1e4c97e69105b15814e1f21c5ff
Alex Bain <alex@alexba.in>
2015-12-30 23:22:41 -0800

    JS now conforms to linting rules

--- app.js
+++ app.js
@@ -1,207 +1,220 @@
 #! /usr/bin/env node
 
 // Requirements
-var express = require('express'),
-    lirc_node = require('lirc_node'),
-    consolidate = require('consolidate'),
-    path = require('path'),
-    swig = require('swig'),
-    labels = require('./lib/labels');
+var express = require('express');
+var lircNode = require('lirc_node');
+var consolidate = require('consolidate');
+var swig = require('swig');
+var labels = require('./lib/labels');
+var https = require('https');
+var fs = require('fs');
 
 // Precompile templates
 var JST = {
-    index: swig.compileFile(__dirname + '/templates/index.swig')
+  index: swig.compileFile(__dirname + '/templates/index.swig'),
 };
 
 // Create app
 var app = module.exports = express();
 
-// App configuration
-app.engine('.html', consolidate.swig);
-app.configure(function() {
-    app.use(express.logger());
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.compress());
-    app.use(express.static(__dirname + '/static'));
-});
-
 // lirc_web configuration
 var config = {};
 
-// Based on node environment, initialize connection to lirc_node or use test data
-if (process.env.NODE_ENV == 'test' || process.env.NODE_ENV == 'development') {
-    lirc_node.remotes = require(__dirname + '/test/fixtures/remotes.json');
-    config = require(__dirname + '/test/fixtures/config.json');
-} else {
-    _init();
-}
+// Server & SSL options
+var port = 3000;
+var sslOptions = {
+  key: null,
+  cert: null,
+};
+
+// Labels for remotes / commands
+var labelFor = labels(config.remoteLabels, config.commandLabels);
+
+// App configuration
+app.engine('.html', consolidate.swig);
+app.configure(function () {
+  app.use(express.logger());
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.compress());
+  app.use(express.static(__dirname + '/static'));
+});
 
 function _init() {
-    lirc_node.init();
+  var home = process.env.HOME;
 
-    // Config file is optional
+  lircNode.init();
+
+  // Config file is optional
+  try {
     try {
-        try {
-          // Check for config file in current directory
-          config = require(__dirname + '/config.json');
-        } catch(e) {
-          var home = process.env['HOME'];
-          config = require(home + '/.lirc_web_config.json');
-        }
-    } catch(e) {
-        console.log("DEBUG:", e);
-        console.log("WARNING: Cannot find config.json!");
+      config = require(__dirname + '/config.json');
+    } catch (e) {
+      config = require(home + '/.lirc_web_config.json');
     }
+  } catch (e) {
+    console.log('DEBUG:', e);
+    console.log('WARNING: Cannot find config.json!');
+  }
 }
 
 function refineRemotes(myRemotes) {
-    function getCommandsForRemote(remoteName) {
-        var commands = myRemotes[remoteName];
+  var newRemotes = {};
+  var newRemoteCommands = null;
+  var remote = null;
 
-        if (isBlacklistExisting(remoteName)){
-            blacklist = config.blacklists[remoteName]
-            commands = commands.filter( function(command) {
-                return blacklist.indexOf(command) < 0;
-            });
-        }
+  function isBlacklistExisting(remoteName) {
+    return config.blacklists && config.blacklists[remoteName];
+  }
 
-        return commands;
+  function getCommandsForRemote(remoteName) {
+    var remoteCommands = myRemotes[remoteName];
+    var blacklist = null;
+
+    if (isBlacklistExisting(remoteName)) {
+      blacklist = config.blacklists[remoteName];
+
+      remoteCommands = remoteCommands.filter(function (command) {
+        return blacklist.indexOf(command) < 0;
+      });
     }
 
-    function isBlacklistExisting(remoteName) {
-        return config.blacklists && config.blacklists[remoteName];
-    }
+    return remoteCommands;
+  }
 
-    var newRemotes = {};
-    for(remote in myRemotes) {
-        commands = getCommandsForRemote(remote);
-        newRemotes[remote] = commands;
-    };
-    return newRemotes;        
-} 
+  for (remote in myRemotes) {
+    newRemoteCommands = getCommandsForRemote(remote);
+    newRemotes[remote] = newRemoteCommands;
+  }
+
+  return newRemotes;
+}
+
+// Based on node environment, initialize connection to lircNode or use test data
+if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
+  lircNode.remotes = require(__dirname + '/test/fixtures/remotes.json');
+  config = require(__dirname + '/test/fixtures/config.json');
+} else {
+  _init();
+}
 
 // Routes
 
-var labelFor = labels(config.remoteLabels, config.commandLabels)
-
-// Web UI
-app.get('/', function(req, res) {
-    var refined_remotes = refineRemotes(lirc_node.remotes);
-    res.send(JST['index'].render({
-        remotes: refined_remotes,
-        macros: config.macros,
-        repeaters: config.repeaters,
-        labelForRemote: labelFor.remote,
-        labelForCommand: labelFor.command
-    }));
+// Index
+app.get('/', function (req, res) {
+  var refinedRemotes = refineRemotes(lircNode.remotes);
+  res.send(JST.index.render({
+    remotes: refinedRemotes,
+    macros: config.macros,
+    repeaters: config.repeaters,
+    labelForRemote: labelFor.remote,
+    labelForCommand: labelFor.command,
+  }));
 });
 
 // Refresh
-app.get('/refresh', function(req, res) {
-    _init();
-    res.redirect('/');
+app.get('/refresh', function (req, res) {
+  _init();
+  res.redirect('/');
 });
 
 // List all remotes in JSON format
-app.get('/remotes.json', function(req, res) {
-    res.json(refineRemotes(lirc_node.remotes));
+app.get('/remotes.json', function (req, res) {
+  res.json(refineRemotes(lircNode.remotes));
 });
 
 // List all commands for :remote in JSON format
-app.get('/remotes/:remote.json', function(req, res) {
-    if (lirc_node.remotes[req.params.remote]) {
-        res.json(refineRemotes(lirc_node.remotes)[req.params.remote]);
-    } else {
-        res.send(404);
-    }
+app.get('/remotes/:remote.json', function (req, res) {
+  if (lircNode.remotes[req.params.remote]) {
+    res.json(refineRemotes(lircNode.remotes)[req.params.remote]);
+  } else {
+    res.send(404);
+  }
 });
 
 // List all macros in JSON format
-app.get('/macros.json', function(req, res) {
-    res.json(config.macros);
+app.get('/macros.json', function (req, res) {
+  res.json(config.macros);
 });
 
 // List all commands for :macro in JSON format
-app.get('/macros/:macro.json', function(req, res) {
-    if (config.macros && config.macros[req.params.macro]) {
-        res.json(config.macros[req.params.macro]);
-    } else {
-        res.send(404);
-    }
+app.get('/macros/:macro.json', function (req, res) {
+  if (config.macros && config.macros[req.params.macro]) {
+    res.json(config.macros[req.params.macro]);
+  } else {
+    res.send(404);
+  }
 });
 
 
 // Send :remote/:command one time
-app.post('/remotes/:remote/:command', function(req, res) {
-    lirc_node.irsend.send_once(req.params.remote, req.params.command, function() {});
-    res.setHeader('Cache-Control', 'no-cache');
-    res.send(200);
+app.post('/remotes/:remote/:command', function (req, res) {
+  lircNode.irsend.send_once(req.params.remote, req.params.command, function () {});
+  res.setHeader('Cache-Control', 'no-cache');
+  res.send(200);
 });
 
 // Start sending :remote/:command repeatedly
-app.post('/remotes/:remote/:command/send_start', function(req, res) {
-    lirc_node.irsend.send_start(req.params.remote, req.params.command, function() {});
-    res.setHeader('Cache-Control', 'no-cache');
-    res.send(200);
+app.post('/remotes/:remote/:command/send_start', function (req, res) {
+  lircNode.irsend.send_start(req.params.remote, req.params.command, function () {});
+  res.setHeader('Cache-Control', 'no-cache');
+  res.send(200);
 });
 
 // Stop sending :remote/:command repeatedly
-app.post('/remotes/:remote/:command/send_stop', function(req, res) {
-    lirc_node.irsend.send_stop(req.params.remote, req.params.command, function() {});
-    res.setHeader('Cache-Control', 'no-cache');
-    res.send(200);
+app.post('/remotes/:remote/:command/send_stop', function (req, res) {
+  lircNode.irsend.send_stop(req.params.remote, req.params.command, function () {});
+  res.setHeader('Cache-Control', 'no-cache');
+  res.send(200);
 });
 
 // Execute a macro (a collection of commands to one or more remotes)
-app.post('/macros/:macro', function(req, res) {
+app.post('/macros/:macro', function (req, res) {
+  var i = 0;
+  var nextCommand = null;
 
-    // If the macro exists, execute each command in the macro with 100msec
-    // delay between each command.
-    if (config.macros && config.macros[req.params.macro]) {
-        var i = 0;
+  // If the macro exists, execute each command in the macro with 100msec
+  // delay between each command.
+  if (config.macros && config.macros[req.params.macro]) {
+    nextCommand = function () {
+      var command = config.macros[req.params.macro][i];
 
-        var nextCommand = function() {
-            var command = config.macros[req.params.macro][i];
+      if (!command) { return true; }
 
-    	    if (!command) { return true; }
+      // increment
+      i = i + 1;
 
-            // increment
-            i = i + 1;
+      if (command[0] === 'delay') {
+        setTimeout(nextCommand, command[1]);
+      } else {
+        // By default, wait 100msec before calling next command
+        lircNode.irsend.send_once(command[0], command[1], function () { setTimeout(nextCommand, 100); });
+      }
+    };
 
-            if (command[0] == "delay") {
-                setTimeout(nextCommand, command[1]);
-            } else {
-                // By default, wait 100msec before calling next command
-                lirc_node.irsend.send_once(command[0], command[1], function() { setTimeout(nextCommand, 100); });
-            }
-        };
+    // kick off macro w/ first command
+    nextCommand();
+  }
 
-        // kick off macro w/ first command
-        nextCommand();
-    }
-
-    res.setHeader('Cache-Control', 'no-cache');
-    res.send(200);
+  res.setHeader('Cache-Control', 'no-cache');
+  res.send(200);
 });
 
 // Listen (http)
-var port = 3000;
-if ( config.server && config.server.port){
-    port = config.server.port;
+if (config.server && config.server.port) {
+  port = config.server.port;
 }
 app.listen(port);
-console.log("Open Source Universal Remote UI + API has started on port " + port + "(http).");
+console.log('Open Source Universal Remote UI + API has started on port ' + port + ' (http).');
 
 // Listen (https)
-if ( config.server && config.server.ssl &&  config.server.ssl_cert && config.server.ssl_key && config.server.ssl_port){
-    var https = require('https');
-    var fs = require('fs');
-    
-    var options = {
-        key: fs.readFileSync(config.server.ssl_key),
-        cert: fs.readFileSync(config.server.ssl_cert)
-    };
-    https.createServer(options, app).listen(config.server.ssl_port);
-    console.log("Open Source Universal Remote UI + API has started on port " + config.server.ssl_port + "(https).");
+if (config.server && config.server.ssl && config.server.ssl_cert && config.server.ssl_key && config.server.ssl_port) {
+  sslOptions = {
+    key: fs.readFileSync(config.server.ssl_key),
+    cert: fs.readFileSync(config.server.ssl_cert),
+  };
+
+  https.createServer(sslOptions, app).listen(config.server.ssl_port);
+
+  console.log('Open Source Universal Remote UI + API has started on port ' + config.server.ssl_port + ' (https).');
 }
