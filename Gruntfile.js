5b03e94841b777ff511d08436db7a34eada7bfc8
Ovis <ovis.maximus@gmail.com>
2016-01-10 11:19:19 +0100

    enabled grunt to continuously run lint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,60 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  // Load some tasks
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-develop');
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
-    // Load some tasks
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-develop');
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      app: {
+        src: ['static/js/vendor/zepto.min.js',
+              'static/js/vendor/zepto.touch.js',
+              'static/js/vendor/fastclick.js',
+              'static/js/app/*.js'],
+        dest: 'static/js/compiled/app.js',
+      },
+    },
 
-        uglify: {
-            app: {
-                src: ['static/js/vendor/zepto.min.js',
-                      'static/js/vendor/zepto.touch.js',
-                      'static/js/vendor/fastclick.js',
-                      'static/js/app/*.js'],
-                dest: 'static/js/compiled/app.js'
-            }
+    less: {
+      app: {
+        files: {
+          'static/css/compiled/app.css': 'static/css/app.less',
         },
+      },
+    },
 
-        less: {
-            app: {
-                files: {
-                    "static/css/compiled/app.css": "static/css/app.less"
-                }
-            }
-        },
+    eslint: {
+      target: ['Gruntfile.js', 'app.js', 'lib/**/*.js', 'test/**/*.js'],
+    },
 
-        watch: {
-            scripts: {
-                files: ['static/js/app/*.js', 'static/js/vendor/*.js'],
-                tasks: ['uglify:app']
-            },
-            stylesheets: {
-                files: ['static/css/*.less'],
-                tasks: ['less']
-            }
-        },
+    watch: {
+      scripts: {
+        files: ['static/js/app/*.js', 'static/js/vendor/*.js'],
+        tasks: ['uglify:app'],
+      },
+      stylesheets: {
+        files: ['static/css/*.less'],
+        tasks: ['less'],
+      },
+      serverscripts: {
+        files: ['<%= eslint.target %>'],
+        tasks: ['eslint'],
+      },
+    },
 
-        develop: {
-            server: {
-                file: 'app.js',
-                env: { NODE_ENV: 'development'}
-            }
-        }
-    });
+    develop: {
+      server: {
+        file: 'app.js',
+        env: { NODE_ENV: 'development' },
+      },
+    },
+  });
 
-    grunt.registerTask('default', ['uglify', 'less']);
-    grunt.registerTask('server', ['uglify', 'less', 'develop', 'watch']);
-
+  grunt.registerTask('default', ['uglify', 'less', 'eslint']);
+  grunt.registerTask('server', ['uglify', 'less', 'eslint', 'develop', 'watch']);
 };
