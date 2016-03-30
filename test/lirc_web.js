4c729261231be3991ee6602e8b5606cf89c22ceb
Chris Simpson <apocolipse@gmail.com>
2016-03-30 00:42:08 -0400

    Linting

--- test/lirc_web.js
+++ test/lirc_web.js
@@ -8,7 +8,7 @@ var jquery = fs.readFileSync('node_modules/jquery/dist/jquery.js', 'utf-8');
 describe('lirc_web', function () {
   describe('routes', function () {
     // Root route
-    it('should have an index route \'/\'', function (done) {
+    it('should have an index route "/"', function (done) {
       assert(request(app).get('/').expect(200, done));
     });
 
