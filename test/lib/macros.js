06646ed2cfaae49e64cb18aabcbf8b43d3031b8b
Chris Simpson <apocolipse@gmail.com>
2016-05-21 20:20:13 -0400

    Added the ability to 'hold' buttons in Macros (#47)
    * Added the ability to 'hold' buttons in Macros
    * Reworked existing tests to pass
    * Added test for hold macro
    * Linting

--- test/lib/macros.js
+++ test/lib/macros.js
@@ -8,7 +8,10 @@ var config = require('../fixtures/config.json');
 describe('macros', function () {
   var lircNode;
   var clock;
-  var stub;
+  var sendOnceStub;
+  var sendStartStub;
+  var sendStopStub;
+
 
   beforeEach(function (done) {
     clock = sinon.useFakeTimers();
@@ -16,8 +19,19 @@ describe('macros', function () {
     lircNode = {};
     lircNode.irsend = {};
     lircNode.irsend.send_once = function () {};
+    lircNode.irsend.send_start = function () {};
+    lircNode.irsend.send_stop = function () {};
 
-    stub = sinon.stub(lircNode.irsend, 'send_once', function (remote, command, cb) {
+
+    sendOnceStub = sinon.stub(lircNode.irsend, 'send_once', function (remote, command, cb) {
+      cb();
+    });
+
+    sendStartStub = sinon.stub(lircNode.irsend, 'send_start', function (remote, command, cb) {
+      cb();
+    });
+
+    sendStopStub = sinon.stub(lircNode.irsend, 'send_stop', function (remote, command, cb) {
       cb();
     });
 
@@ -34,29 +48,46 @@ describe('macros', function () {
     it('should call lircNode.irsend.send_once when executing a macro', function () {
       macros.exec(config.macros['Play Xbox 360'], lircNode);
 
-      assert.equal(stub.called, true);
+      assert.equal(sendOnceStub.called, true);
     });
 
     it('should delay when encountering a delay', function () {
       macros.exec(config.macros['Macro With Delay'], lircNode);
 
-      assert.equal(stub.called, false);
+      assert.equal(sendOnceStub.called, false);
 
-      // not enough time has passed, stub should not have been called
+      // not enough time has passed, sendOnceStub should not have been called
       clock.tick(250);
-      assert.equal(stub.called, false);
+      assert.equal(sendOnceStub.called, false);
 
       // enough time has now passed, macro should have tried to execute next command
       clock.tick(250);
-      assert.equal(stub.called, true);
+      assert.equal(sendOnceStub.called, true);
     });
 
     it('should call send_once once per command', function () {
       macros.exec(config.macros['Play Xbox 360'], lircNode);
       // wait enough time
       clock.tick(500);
+      assert.equal(sendOnceStub.callCount, 5);
+    });
 
-      assert.equal(stub.callCount, 5);
+    it('should call send_start and send_stop at least once', function () {
+      macros.exec(config.macros['XboxOne Off'], lircNode);
+
+      assert.equal(sendStartStub.called, true);
+
+      // sendStopStub shouldn't have been called yet
+      assert.equal(sendStopStub.called, false);
+
+      // wait
+      clock.tick(1600);
+
+      assert.equal(sendStopStub.called, true);
+
+      // Wait some more before checking the last methods were called
+      clock.tick(200);
+      assert.equal(sendOnceStub.callCount, 2);
     });
   });
 });
